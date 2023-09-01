0f943fc1f50e2c88582e8d43437495c5ee41cced
yanatoritakuma <yanatori333@gmail.com>
2023-09-01 20:53:45 +0900

    add:sc

--- router/router.go
+++ router/router.go
@@ -16,7 +16,7 @@ func NewRouter(
 ) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		AllowOrigins: []string{"http://localhost:3000", os.Getenv("FE_URL"), os.Getenv("FE_SC_URL")},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
 			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
